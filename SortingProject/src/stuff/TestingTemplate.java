82b628a69b730702bcc2c7941e6987ff2f3b4d5c
garri <garri@Yoga-720>
2017-12-02 18:08:42 -0600

    Got the sorter template working

---
+++ SortingProject/src/stuff/TestingTemplate.java
@@ -0,0 +1,296 @@
+package stuff;
+
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TestingTemplate {
+
+	public static void main(String[] args) {
+
+		int fileNum;
+		String pathString;
+		Scanner scan;
+		int currPos;
+
+		int[][] rand1 = new int[10][10000];
+		int[][] rand2 = new int[10][20000];
+		int[][] rand3 = new int[10][30000];
+		int[][] rand4 = new int[10][40000];
+		int[][] rand5 = new int[10][50000];
+
+		int[][] active;
+
+		for (int i = 10000; i <= 50000; i += 10000) {
+
+			for (fileNum = 1; fileNum <= 10; fileNum++) {
+
+				pathString = "C:\\Code\\RandomNum\\" + Integer.toString(i) + "\\rand" + Integer.toString(fileNum)
+						+ ".txt";
+
+				Path path = Paths.get(pathString);
+
+				switch (i) {
+				case 10000:
+					active = rand1;
+					break;
+				case 20000:
+					active = rand2;
+					break;
+				case 30000:
+					active = rand3;
+					break;
+				case 40000:
+					active = rand4;
+					break;
+				case 50000:
+					active = rand5;
+					break;
+				default:
+					active = rand1;
+				}
+
+				try {
+					scan = new Scanner(path);
+					currPos = 0;
+
+					while (scan.hasNextInt()) {
+						active[fileNum - 1][currPos] = scan.nextInt();
+						currPos++;
+					}
+
+				} catch (Exception ignored) {
+				}
+
+			}
+		}
+
+		/////////////////////////////////////////////////////////////
+		// Insert the sorting algorithm here
+		/////////////////////////////////////////////////////////////
+
+		// columns are for the 10 rand files, the rows indicate which length are being
+		// used
+		long[][] times = new long[5][10];
+
+		// stores the start, final, and elapsed time
+		long startTime;
+		long finalTime;
+		long time;
+
+		// holds the current array being sorted
+		int currArray[];
+
+		// contains the sorts
+		SortsClass sorts = new SortsClass();
+
+		// holds the sorted data
+		Integer[] data;
+
+		// rand1
+		for (int i = 1; i <= 10; i++) {
+
+			currArray = rand1[i - 1];
+
+			data = Arrays.stream(currArray).boxed().toArray(Integer[]::new);
+
+			startTime = System.nanoTime();
+
+			sorts.selectionSort(data, 10000);
+
+			finalTime = System.nanoTime();
+			time = finalTime - startTime;
+
+			System.out.println(i + ": " + time);
+			times[0][i - 1] = time;
+
+			if (i == 10) {
+				try {
+					Files.createFile(Paths.get("C:\\Code\\sortedRand10_10000.txt"));
+				} catch (Exception ignored) {
+				}
+				try {
+					PrintWriter out = new PrintWriter("C:\\Code\\sortedRand10_10000.txt");
+
+					for (Integer intInData : data) {
+						out.println(intInData);
+					}
+
+				} catch (Exception ignored) {
+				}
+			}
+		}
+
+		double sum = 0;
+		for (long i : times[0]) {
+			sum += i;
+		}
+		System.out.println("The average time (10000) was " + Double.toString(((sum / 10) / 1000000000)) + " seconds.");
+
+		// rand2
+		for (int i = 1; i <= 10; i++) {
+
+			currArray = rand2[i - 1];
+
+			data = Arrays.stream(currArray).boxed().toArray(Integer[]::new);
+
+			startTime = System.nanoTime();
+
+			sorts.selectionSort(data, 20000);
+
+			finalTime = System.nanoTime();
+			time = finalTime - startTime;
+
+			System.out.println(i + ": " + time);
+			times[1][i - 1] = time;
+
+			if (i == 10) {
+				try {
+					Files.createFile(Paths.get("C:\\Code\\sortedRand10_20000.txt"));
+				} catch (Exception ignored) {
+				}
+				try {
+					PrintWriter out = new PrintWriter("C:\\Code\\sortedRand10_20000.txt");
+
+					for (Integer intInData : data) {
+						out.println(intInData);
+					}
+
+				} catch (Exception ignored) {
+				}
+			}
+		}
+
+		sum = 0;
+		for (long i : times[0]) {
+			sum += i;
+		}
+		System.out.println("The average time (20000) was " + Double.toString(((sum / 10) / 1000000000)) + " seconds.");
+		
+		// rand3
+		for (int i = 1; i <= 10; i++) {
+
+			currArray = rand3[i - 1];
+
+			data = Arrays.stream(currArray).boxed().toArray(Integer[]::new);
+
+			startTime = System.nanoTime();
+
+			sorts.selectionSort(data, 30000);
+
+			finalTime = System.nanoTime();
+			time = finalTime - startTime;
+
+			System.out.println(i + ": " + time);
+			times[2][i - 1] = time;
+
+			if (i == 10) {
+				try {
+					Files.createFile(Paths.get("C:\\Code\\sortedRand10_30000.txt"));
+				} catch (Exception ignored) {
+				}
+				try {
+					PrintWriter out = new PrintWriter("C:\\Code\\sortedRand10_30000.txt");
+
+					for (Integer intInData : data) {
+						out.println(intInData);
+					}
+
+				} catch (Exception ignored) {
+				}
+			}
+		}
+
+		sum = 0;
+		for (long i : times[0]) {
+			sum += i;
+		}
+		System.out.println("The average time (30000) was " + Double.toString(((sum / 10) / 1000000000)) + " seconds.");
+		
+		// rand4
+		for (int i = 1; i <= 10; i++) {
+
+			currArray = rand4[i - 1];
+
+			data = Arrays.stream(currArray).boxed().toArray(Integer[]::new);
+
+			startTime = System.nanoTime();
+
+			sorts.selectionSort(data, 40000);
+
+			finalTime = System.nanoTime();
+			time = finalTime - startTime;
+
+			System.out.println(i + ": " + time);
+			times[3][i - 1] = time;
+
+			if (i == 10) {
+				try {
+					Files.createFile(Paths.get("C:\\Code\\sortedRand10_40000.txt"));
+				} catch (Exception ignored) {
+				}
+				try {
+					PrintWriter out = new PrintWriter("C:\\Code\\sortedRand10_40000.txt");
+
+					for (Integer intInData : data) {
+						out.println(intInData);
+					}
+
+				} catch (Exception ignored) {
+				}
+			}
+		}
+
+		sum = 0;
+		for (long i : times[0]) {
+			sum += i;
+		}
+		System.out.println("The average time (40000) was " + Double.toString(((sum / 10) / 1000000000)) + " seconds.");
+		
+		// rand5
+		for (int i = 1; i <= 10; i++) {
+
+			currArray = rand5[i - 1];
+
+			data = Arrays.stream(currArray).boxed().toArray(Integer[]::new);
+
+			startTime = System.nanoTime();
+
+			sorts.selectionSort(data, 50000);
+
+			finalTime = System.nanoTime();
+			time = finalTime - startTime;
+
+			System.out.println(i + ": " + time);
+			times[4][i - 1] = time;
+
+			if (i == 10) {
+				try {
+					Files.createFile(Paths.get("C:\\Code\\sortedRand10_50000.txt"));
+				} catch (Exception ignored) {
+				}
+				try {
+					PrintWriter out = new PrintWriter("C:\\Code\\sortedRand10_50000.txt");
+
+					for (Integer intInData : data) {
+						out.println(intInData);
+					}
+
+				} catch (Exception ignored) {
+				}
+			}
+		}
+
+		sum = 0;
+		for (long i : times[0]) {
+			sum += i;
+		}
+		System.out.println("The average time (50000) was " + Double.toString(((sum / 10) / 1000000000)) + " seconds.");
+		/////////////////////////////////////////////////////////////
+		// End of sorting algorithm
+		/////////////////////////////////////////////////////////////
+	}
+}
